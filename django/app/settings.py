4fab8d4cd990eb728c89336686b73b6e8e2a28ab
Renato Pedigoni <renatopedigoni@gmail.com>
2013-01-24 10:55:07 -0200

    added admins

--- django/app/settings.py
+++ django/app/settings.py
@@ -6,7 +6,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Renato', 'renatopedigoni@gmail.com'),
 )
 
 AUTH_USER_MODEL = 'accounts.User'
